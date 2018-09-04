c27f251d3ee5e1a61c596402d5dab58a1b280660
Guglielmetti Philippe <philippe.guglielmetti@bobst.com>
2018-09-04 09:04:18 +0200

    updated for webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,49 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// webpack v4
+// see https://hackernoon.com/a-tale-of-webpack-4-and-how-to-finally-configure-it-in-the-right-way-4e94c8e7e5c1
+
+const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 var PRODUCTION = process.argv[2] === '-p';
+
 
 module.exports = {
   entry: require('./webpack/entries')(PRODUCTION),
   output: {
-    publicPath: '',
-    path: 'dist/',
+    path: __dirname + "/dist",
     filename: '[name].js',
     library: 'timeline',
     libraryTarget: 'umd'
   },
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$/, include: __dirname + '/style.css', loader: ExtractTextPlugin.extract('css') },
-    ],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
+
   externals: {
     d3: 'd3',
   },
-  plugins: require('./webpack/plugins')(PRODUCTION),
+  plugins: [
+    require('./webpack/plugins')(PRODUCTION),
+    // new ExtractTextPlugin(
+    //   {filename: 'style.css', disable: false, allChunks: true }
+    // ),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+  ]
 };
